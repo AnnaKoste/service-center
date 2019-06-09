25185c6494e786a9947a78bf06b1e92a048d5b90
Anna <kostechuk.anna@gmail.com>
2019-06-09 14:54:13 +0800

    change favicon

--- src/price.js
+++ src/price.js
@@ -60,36 +60,35 @@ class Table extends React.Component {
       shown: !this.state.shown,
     });
     if (this.state.symbol === '⋁') {
-        this.setState({symbol: '∧'});
+      this.setState({symbol: '∧'});
     } else {
       this.setState({symbol: '⋁'});
     }
   }
 
   render() {
-
     return (
       <div>
-            <table>
-              <thead className="price-header" onClick={this.toggle.bind(this)} >
-                <tr>
-                  <th className="table-header long">{this.state.data.type}</th>
-                  <th className="short">от {this.state.data.maxTerm} минут</th>
-                  <th className="short"> </th>
-                  <th className="table-header price-header-right short">{this.state.symbol}</th>
-                </tr>
-              </thead>
-              <tbody hidden={ this.state.shown }>
-                {this.state.data.options.map(op => (
-                  <tr>
-                    <td className="long">{op.name}</td>
-                    <td className="text-green short">от {op.term} минут</td>
-                    <td className="text-green short">{op.cost}</td>
-                    <td className="text-blue short">Записаться</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        <table>
+          <thead className="price-header" onClick={this.toggle.bind(this)} >
+            <tr>
+              <th className="table-header long">{this.state.data.type}</th>
+              <th className="short">от {this.state.data.maxTerm} минут</th>
+              <th className="short"> </th>
+              <th className="table-header price-header-right short">{this.state.symbol}</th>
+            </tr>
+          </thead>
+          <tbody hidden={ this.state.shown }>
+            {this.state.data.options.map(op => (
+              <tr>
+                <td className="long">{op.name}</td>
+                <td className="text-green short">от {op.term} минут</td>
+                <td className="text-green short">{op.cost}</td>
+                <td className="text-blue short">Записаться</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     )
   }
